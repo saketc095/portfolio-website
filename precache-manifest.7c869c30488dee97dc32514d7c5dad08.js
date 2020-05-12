e3c0c20a2e5cd77a22f4a83c7f08312a655f1666
lalitmanwani <lalit631995@gmail.com>
2020-05-12 15:02:35 -0400

    Updates

---
+++ precache-manifest.7c869c30488dee97dc32514d7c5dad08.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e6ddfe21f35b91da45dba8bd4b8b1d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a245f1df47bc6632f817",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "91b3792519667d6cbe10",
+    "url": "/portfolio/static/css/main.a5cdace4.chunk.css"
+  },
+  {
+    "revision": "a245f1df47bc6632f817",
+    "url": "/portfolio/static/js/2.a1c95c7b.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.a1c95c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b3792519667d6cbe10",
+    "url": "/portfolio/static/js/main.c2dc1438.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "7d4837cc51ff62fc7bd3831df6e1735a",
+    "url": "/portfolio/static/media/Resume.7d4837cc.pdf"
+  },
+  {
+    "revision": "c14f7b2f23af4b12a97c7b36f2390020",
+    "url": "/portfolio/static/media/myPic.c14f7b2f.jpg"
+  }
+]);
