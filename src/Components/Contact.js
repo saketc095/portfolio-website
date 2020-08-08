3ee1baa83d140d7d23b3e957537ab7c4c8254be9
lalitmanwani <lalit631995@gmail.com>
2020-08-08 15:23:40 -0400

    change

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,48 +1,57 @@
-import React,{Component} from 'react';
-import { Grid, Cell } from 'react-mdl';
-import myPic from '../myPic.jpg';
-import Resume from '../Resume.pdf'
-import {Link} from 'react-scroll';
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 
+class Contact extends Component {
+  render() {
+    return(
+      <div className="contact-body" id="contact">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Saket Chinchalikar</h2>
+            <img
+              src="https://cdn1.iconfinder.com/data/icons/faces-solid-1/48/48-512.png"
+              alt="avatar"
+              style={{height: '250px'}}
+               />
+             <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>Want to get in touch? I'd love to hear from you. Here's how you can reach to me</p>
 
-class LandingPage extends Component{
-    render(){
-        return(
-            <div id="lp">
-                <Grid className="landing-page" >
-                <Link style={{color:'white', margin:'auto'}} activeClass="active" to="abt" spy={true} smooth={true} offset={0} duration= {1000}>ABOUT</Link>
-                <Link style={{color:'white', margin:'auto'}} activeClass="active" to="timeline" spy={true} smooth={true} offset={0} duration= {1000}>TIMELINE </Link>                
-                <Link style={{color:'white', margin:'auto'}} activeClass="active" to="contact" spy={true} smooth={true} offset={0} duration= {1000}>CONTACT </Link>   
-                    <Cell col={12}>
-                        <img className="myPic" src = {myPic} alt="mypic"/>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
 
-                        <div className="title">
-                            <h1>Hi, <br />I'm Saket Chinchalikar</h1>
-                            <p className="resume-text"><a href={Resume} target="blank">View Resume</a></p>
-                            <br></br>
-                            
-                            <p> Full Stack Developer | Software Engineer | Coder</p>
-                            <div className="social-links">
-                                <a href="https://www.facebook.com/saket.chinchalikar.1" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-facebook-square" aria-hidden="true" /></a>
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                    +1(607)-232-8987
+                  </ListItemContent>
+                </ListItem>
 
-                                <a href="https://www.linkedin.com/in/saket-chinchalikar/" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-linkedin" aria-hidden="true" /></a>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                    saketc095@gmail.com
+                  </ListItemContent>
+                </ListItem>
 
-                                <a href="https://www.instagram.com/saket9_c/" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-instagram" aria-hidden="true" /></a>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-linkedin" aria-hidden="true"/>
+                    <a style={{textDecoration: 'none', color: 'black'}} href="https://www.linkedin.com/in/saket-chinchalikar/" target="_blank">Saket Chinchalikar</a>
+                  </ListItemContent>
+                </ListItem>
 
-                                <a href="https://github.com/saketc095" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-github" aria-hidden="true" /></a>
-                            </div>
-                        </div>
-                    </Cell>
-                </Grid>
-                
+
+              </List>
             </div>
-        )
-    }
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
 }
 
-export default LandingPage
+export default Contact;
